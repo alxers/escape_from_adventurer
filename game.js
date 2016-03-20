dfbf84331c210fe5fe4bfce02450cc2068617a2f
Alexey Ershov <alex4rom@gmail.com>
2016-03-20 12:30:43 +0300

    Create Game class (refactoring)

--- game.js
+++ game.js
@@ -17,287 +17,270 @@ class Location {
 class Game {
 
     constructor() {
-        this.map = [
-            new Place('Fire control station'),
-            new Place('Captains room'),
-            new Place('Maintanance station'),
-            new Place('Ship crew quarters'),
-            new Place('Main bridge'),
-            new Place('Sick bay'),
-            new Place('Cargo bay'),
-            new Place('Drive shaft'),
-            new Place('Rescue boats')
+
+        // Other possible items
+        // 'Energy Shield',
+        // 'Strength Gauntlet',
+        // 'Combat Suit',
+        // 'Light Exoskeleton',
+        // 'Blaster Pistol',
+        // 'Ion Blaster',
+        // 'Sonic Grenade',
+        // 'Energy Cell',
+        // 'Medpac',
+        // 'Life Support Pack',
+        // 'Antidote Kit',
+        // 'Adrenal Strength Stimulant',
+        // 'Repair Kit'
+        this.locationItems = [
+            { 'Fire control station': ['energy cell'] },
+            { 'Captains room': null },
+            { 'Maintanance station': ['repair kit'] },
+            { 'Ship crew quarters': ['breath mask'] },
+            { 'Main bridge': null },
+            { 'Sick bay': null },
+            { 'Cargo bay': null },
+            { 'Drive shaft': null },
+            { 'Rescue boats': null }
+
         ];
+
+        this.map = this.locationItems.map(function(locItem) {
+            let loc = Object.keys(locItem)[0];
+            return new Location(loc, Utils.toSnakeCase(loc), locItem[loc])
+        });
+
+        this.knownActions = [
+            'forward',
+            'right',
+            'back',
+            'left',
+            'take',
+            'use rescue boat',
+            'use',
+            'drop',
+            'help',
+            'inventory'
+        ];
+
+        this.action = '';
+        this.item = '';
 
         this.inventory = [];
         this.currentPosition = 0;
+
+        this.blockedPathMessages = ['There is nothing there, only space'];
+
+        // Quest stuff
+        this.energyCellInstalled = false;
+        this.boatRepaired = false;
+
+        this.mapLocation = 4;
+        this.playersInput = '';
+        this.gameMessage = '';
+
+        // Do I need this?
+        // this.itemLocations = [0, 1, 2, 3]; // 5, 6
+
+        this.boardEl = document.getElementById('board');
+        this.inputEl = document.getElementById('input');
+        this.enterBtnEl = document.getElementById('enterBtn');
+        this.imgEl = document.getElementById('ship-image');
+
+        this.enterBtnEl.addEventListener('click', playGame, false);
+        this.inputEl.addEventListener('keydown', function(e){ if (e.keyCode === 13){ playGame() }}, false);
+
+    }
+
+    showInventory() {
+        if (inventory.length) {
+            gameMessage = 'You have ' + inventory.join(', ') + ' in your inventory';
+        } else {
+            gameMessage = 'Nothing in there';
+        }
+    }
+
+    takeItem() {
+        let itemIndex = items.indexOf(item);
+
+        if (itemIndex !== - 1 && itemLocations[itemIndex] === mapLocation) {
+            gameMessage = 'You take the ' + item;
+            inventory.push(item);
+            items.splice(itemIndex, 1);
+            itemLocations.splice(itemIndex, 1);
+        } else {
+            gameMessage = 'Unknown input';
+        }
+    }
+
+    dropItem() {
+        if (inventory.length !== 0) {
+            var inventoryIndex = inventory.indexOf(item);
+
+            if (inventoryIndex !== -1) {
+                gameMessage = 'You drop the ' + item;
+                items.push(inventory[inventoryIndex]);
+                itemLocations.push(mapLocation);
+
+                inventory.splice(inventoryIndex, 1);
+            } else {
+                gameMessage = 'You cant do that';
+            }
+        } else {
+            gameMessage = 'You are not carrying anything';
+        }
+    }
+
+    // TODO: get rid of magic numbers
+    // add real items
+    useItem() {
+        var inventoryIndex = inventory.indexOf(item);
+
+        if (inventoryIndex === -1) {
+            gameMessage = 'You are not carrying it';
+        }
+
+        if (inventory.length === 0) {
+            gameMessage += ' Your inventory is empty';
+        }
+
+        if (inventoryIndex !== -1) {
+            switch(item) {
+                case 'energy cell':
+                    if (mapLocation === 8) {
+                        energyCellInstalled = true;
+                        gameMessage = 'You\'ve installed energy cell';
+                        inventory.splice(inventoryIndex, 1);
+                    } else {
+                        gameMessage = 'It\'s no use in here';
+                    }
+                    break;
+                case 'repair kit':
+                    if (mapLocation === 8) {
+                        boatRepaired = true;
+                        gameMessage = 'You\'ve repaired the boat';
+                        inventory.splice(inventoryIndex, 1);
+                    } else {
+                        gameMessage = 'It\'s no use in here';
+                    }
+                    break;
+                default:
+                    gameMessage = 'Unknown input';
+            }
+        }
+    }
+
+    gameComplete() {
+        if ((mapLocation === 8) && energyCellInstalled && boatRepaired) {
+            gameMessage = 'You successfully escaped in the resque boat';
+        } else {
+            gameMessage = 'You cant do that';
+        }
+    }
+
+    // Move to Utils?
+    showHelpMessage() {
+        gameMessage = 'You can type in: ' + knownActions.join(', ');
+    }
+
+    render();
+
+    playGame() {
+        playersInput = inputEl.value.toLowerCase();
+
+        gameMessage = '';
+        action = '';
+
+        for (let i = 0; i < knownActions.length; i++) {
+            if (playersInput.indexOf(knownActions[i]) !== -1) {
+                action = knownActions[i];
+                if (playersInput === knownActions[i]) {
+                    action = knownActions[i];
+                }
+                console.log('players action: ' + action);
+                break;
+            }
+        }
+
+        for (let i = 0; i < knownItems.length; i++) {
+          if (playersInput.indexOf(knownItems[i]) !== -1) {
+            item = knownItems[i];
+          }
+        }
+
+        switch (action) {
+            case 'forward':
+                if (mapLocation >= 3) {
+                    mapLocation -= 3;
+                } else {
+                    gameMessage = blockedPathMessages[0];
+                }
+                break;
+            case 'right':
+                if (mapLocation % 3 !== 2) {
+                    mapLocation += 1;
+                } else {
+                    gameMessage = blockedPathMessages[0];
+                }
+                break;
+            case 'back':
+                if (mapLocation < 6) {
+                    mapLocation += 3;
+                } else {
+                    gameMessage = blockedPathMessages[0];
+                }
+                break;
+            case 'left':
+                if (mapLocation % 3 !== 0) {
+                    mapLocation -= 1;
+                } else {
+                    gameMessage = blockedPathMessages[0];
+                }
+                break;
+            case 'take':
+                takeItem();
+                break;
+            case 'drop':
+                dropItem();
+                break;
+            case 'use rescue boat':
+                gameComplete();
+                break;
+            case 'use':
+                useItem();
+                break;
+            case 'help':
+                showHelpMessage();
+                break;
+            case 'inventory':
+                showInventory();
+                break;
+            default:
+                gameMessage = 'Unknown input';
+        }
+
+        render();
+
+    }
+
+    render() {
+        boardEl.innerHTML = map[mapLocation];
+        imgEl.src = './images/' + images[mapLocation];
+
+        // TODO: fix "You see a 'null' here if I drop an item"
+        for (let i = 0; i < items.length; i++) {
+            if (mapLocation === itemLocations[i]) {
+                boardEl.innerHTML += '<br>You see a <strong>' + items[i] + '</strong> here.';
+            }
+        }
+
+        boardEl.innerHTML += '<br>' + gameMessage;
+
+        if (inventory.length !== 0) {
+            boardEl.innerHTML += '<br>You are carrying: ' + inventory.join(', ');
+        }
+
+        inputEl.value = '';
     }
 
 }
-
-// blockedPathMessages = [
-//         'There is nothing there, only space'
-//     ];
-
-// var items = [
-//     'energy cell',
-//     null, // 'access key'
-//     'repair kit',
-//     'breath mask',
-//     null,
-//     null, // 'Vaccine',
-//     null, // 'Explosion',
-//     null,
-//     null
-//    // 'Energy Shield',
-//    // 'Strength Gauntlet',
-//    // 'Combat Suit',
-//    // 'Light Exoskeleton',
-//    // 'Blaster Pistol',
-//    // 'Ion Blaster',
-//    // 'Sonic Grenade',
-//    // 'Energy Cell',
-//    // 'Medpac',
-//    // 'Life Support Pack',
-//    // 'Antidote Kit',
-//    // 'Adrenal Strength Stimulant',
-//    // 'Repair Kit'
-// ];
-
-// var energyCellInstalled = false;
-// var boatRepaired = false;
-
-// // Get rid of the same array (require changing the for loop in the playGame function
-// var knownItems = [
-//     'energy cell',
-//     null,
-//     'repair kit',
-//     'breath mask',
-//     null,
-//     null, // 'Vaccine',
-//     null, // 'Explosion',
-//     null,
-//     null
-//     ];
-
-// var itemLocations = [0, 1, 2, 3]; // 5, 6
-
-// var inventory = [];
-
-// var mapLocation = 4;
-// var playersInput = '';
-// var gameMessage = '';
-
-// var knownActions = [
-//     'forward',
-//     'right',
-//     'back',
-//     'left',
-//     'take',
-//     'use rescue boat',
-//     'use',
-//     'drop',
-//     'help',
-//     'inventory'
-//     ];
-// var action = '';
-
-// var item = '';
-
-// var boardEl = document.getElementById('board');
-// var inputEl = document.getElementById('input');
-// var enterBtnEl = document.getElementById('enterBtn');
-// var imgEl = document.getElementById('ship-image');
-
-// enterBtnEl.addEventListener('click', playGame, false);
-// inputEl.addEventListener('keydown', function(e){ if (e.keyCode === 13){ playGame() }}, false);
-
-// function showInventory() {
-//     if (inventory.length) {
-//         gameMessage = 'You have ' + inventory.join(', ') + ' in your inventory';
-//     } else {
-//         gameMessage = 'Nothing in there';
-//     }
-// }
-
-// function takeItem() {
-//     var itemIndex = items.indexOf(item);
-
-//     if (itemIndex !== - 1 && itemLocations[itemIndex] === mapLocation) {
-//         gameMessage = 'You take the ' + item;
-//         inventory.push(item);
-//         items.splice(itemIndex, 1);
-//         itemLocations.splice(itemIndex, 1);
-//     } else {
-//         gameMessage = 'Unknown input';
-//     }
-// }
-
-// function dropItem() {
-//     if (inventory.length !== 0) {
-//         var inventoryIndex = inventory.indexOf(item);
-
-//         if (inventoryIndex !== -1) {
-//             gameMessage = 'You drop the ' + item;
-//             items.push(inventory[inventoryIndex]);
-//             itemLocations.push(mapLocation);
-
-//             inventory.splice(inventoryIndex, 1);
-//         } else {
-//             gameMessage = 'You cant do that';
-//         }
-//     } else {
-//         gameMessage = 'You are not carrying anything';
-//     }
-// }
-
-// // TODO: get rid of magic numbers
-// // add real items
-// function useItem() {
-//     var inventoryIndex = inventory.indexOf(item);
-
-//     if (inventoryIndex === -1) {
-//         gameMessage = 'You are not carrying it';
-//     }
-
-//     if (inventory.length === 0) {
-//         gameMessage += ' Your inventory is empty';
-//     }
-
-//     if (inventoryIndex !== -1) {
-//         switch(item) {
-//             case 'energy cell':
-//                 if (mapLocation === 8) {
-//                     energyCellInstalled = true;
-//                     gameMessage = 'You\'ve installed energy cell';
-//                     inventory.splice(inventoryIndex, 1);
-//                 } else {
-//                     gameMessage = 'It\'s no use in here';
-//                 }
-//                 break;
-//             case 'repair kit':
-//                 if (mapLocation === 8) {
-//                     boatRepaired = true;
-//                     gameMessage = 'You\'ve repaired the boat';
-//                     inventory.splice(inventoryIndex, 1);
-//                 } else {
-//                     gameMessage = 'It\'s no use in here';
-//                 }
-//                 break;
-//             default:
-//                 gameMessage = 'Unknown input';
-//         }
-//     }
-// }
-
-// function gameComplete() {
-//     if ((mapLocation === 8) && energyCellInstalled && boatRepaired) {
-//         gameMessage = 'You successfully escaped in the resque boat';
-//     } else {
-//         gameMessage = 'You cant do that';
-//     }
-// }
-
-// function showHelpMessage() {
-//     gameMessage = 'You can type in: ' + knownActions.join(', ');
-// }
-
-// render();
-
-// function playGame() {
-//     playersInput = inputEl.value.toLowerCase();
-
-//     gameMessage = '';
-//     action = '';
-
-//     for (let i = 0; i < knownActions.length; i++) {
-//         if (playersInput.indexOf(knownActions[i]) !== -1) {
-//             action = knownActions[i];
-//             if (playersInput === knownActions[i]) {
-//                 action = knownActions[i];
-//             }
-//             console.log('players action: ' + action);
-//             break;
-//         }
-//     }
-
-//     for (let i = 0; i < knownItems.length; i++) {
-//       if (playersInput.indexOf(knownItems[i]) !== -1) {
-//         item = knownItems[i];
-//       }
-//     }
-
-//     switch (action) {
-//         case 'forward':
-//             if (mapLocation >= 3) {
-//                 mapLocation -= 3;
-//             } else {
-//                 gameMessage = blockedPathMessages[0];
-//             }
-//             break;
-//         case 'right':
-//             if (mapLocation % 3 !== 2) {
-//                 mapLocation += 1;
-//             } else {
-//                 gameMessage = blockedPathMessages[0];
-//             }
-//             break;
-//         case 'back':
-//             if (mapLocation < 6) {
-//                 mapLocation += 3;
-//             } else {
-//                 gameMessage = blockedPathMessages[0];
-//             }
-//             break;
-//         case 'left':
-//             if (mapLocation % 3 !== 0) {
-//                 mapLocation -= 1;
-//             } else {
-//                 gameMessage = blockedPathMessages[0];
-//             }
-//             break;
-//         case 'take':
-//             takeItem();
-//             break;
-//         case 'drop':
-//             dropItem();
-//             break;
-//         case 'use rescue boat':
-//             gameComplete();
-//             break;
-//         case 'use':
-//             useItem();
-//             break;
-//         case 'help':
-//             showHelpMessage();
-//             break;
-//         case 'inventory':
-//             showInventory();
-//             break;
-//         default:
-//             gameMessage = 'Unknown input';
-//     }
-
-//     render();
-
-// }
-
-// function render() {
-//     boardEl.innerHTML = map[mapLocation];
-//     imgEl.src = './images/' + images[mapLocation];
-
-//     // TODO: fix "You see a 'null' here if I drop an item"
-//     for (let i = 0; i < items.length; i++) {
-//         if (mapLocation === itemLocations[i]) {
-//             boardEl.innerHTML += '<br>You see a <strong>' + items[i] + '</strong> here.';
-//         }
-//     }
-
-//     boardEl.innerHTML += '<br>' + gameMessage;
-
-//     if (inventory.length !== 0) {
-//         boardEl.innerHTML += '<br>You are carrying: ' + inventory.join(', ');
-//     }
-
-//     inputEl.value = '';
-// }
